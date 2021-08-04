5543593204a280eb0f51b7d86d5bd6726936a899
Prajwalgn-07 <prajwalg@testvagrant.com>
2021-08-04 11:58:17 +0530

    added properties file

--- src/test/java/ThirdClassAssignmentTestScripts/PostApiAutomation.java
+++ src/test/java/ThirdClassAssignmentTestScripts/PostApiAutomation.java
@@ -4,20 +4,32 @@ import ApiResources.postDetailsOfUserRegistration.PostDetailsOfProduct;
 import ApiResources.postDetailsOfUserRegistration.RequestDetailsOfProduct;
 import ApiResources.postDetailsOfUserRegistration.Response.PostDetailsOfProductResponse;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 public class PostApiAutomation {
+    Properties properties = new Properties();
+    @BeforeMethod
+    public void setProperty(){
+        try (InputStream input = new FileInputStream("/Users/prajwal/Desktop/testvagrant /assignment projects/ApiAutomationPractice/src/main/java/properties/productDetails.properties")) {
+            properties.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
     @Test
     public void postApiAddingTheProduct() {
         RequestDetailsOfProduct requestDetailsOfProduct=new RequestDetailsOfProduct();
-        requestDetailsOfProduct.setTitle("test product");
-        requestDetailsOfProduct.setCategory("electronic");
-        requestDetailsOfProduct.setPrice("13.5");
-        requestDetailsOfProduct.setDescription("lorem ipsum set");
-        requestDetailsOfProduct.setImage("https://i.pravatar.cc");
+        requestDetailsOfProduct.setTitle(properties.getProperty("title"));
+        requestDetailsOfProduct.setCategory(properties.getProperty("category"));
+        requestDetailsOfProduct.setPrice(properties.getProperty("price"));
+        requestDetailsOfProduct.setDescription(properties.getProperty("description"));
+        requestDetailsOfProduct.setImage(properties.getProperty("imageLink"));
         PostDetailsOfProductResponse postDetailsOfProductResponse=new PostDetailsOfProduct().postDetailsOfProduct(requestDetailsOfProduct,"https://fakestoreapi.com/products");
         List<String>list=new ArrayList<>();
         list.add("1");
